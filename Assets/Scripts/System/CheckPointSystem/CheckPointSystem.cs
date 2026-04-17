1e0d0c810f67d878d452507bdd838f84bcc27b33
SumeragiH <1443490671@qq.com>
2026-04-17 21:41:21 +0800

    检查点优化

--- Assets/Scripts/System/CheckPointSystem/CheckPointSystem.cs
+++ Assets/Scripts/System/CheckPointSystem/CheckPointSystem.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class CheckPointSystem : SingletonBaseWithMono<CheckPointSystem>
 {
-    //存储检查点的栈，每次保存检查点时，将数据压入栈中，每次加载检查点时，从栈顶弹出数据
-    public Stack<CheckPointData> checkPoints = new Stack<CheckPointData>();
+    //一个存储检查点数据的字典，键为检查点名称，值为对应的检查点数据
+    public Dictionary<string, CheckPointData> checkPoints = new Dictionary<string, CheckPointData>();
+    public string currentCheckPointName;//当前检查点的名称，可以根据需要进行设置和使用
 
-    public void SaveCheckPoint()
+    public void SaveCheckPoint(string CheckPointName)
     {
+        currentCheckPointName = CheckPointName;
         // 获取玩家当前的位置和状态等信息，创建一个新的CheckPointData对象，并将其压入栈中
         CheckPointData checkPointData = new CheckPointData();
+        checkPointData.playerPosition = PlayerView.Instance.transform.position;
+        checkPointData.scanPoint = CollectionSystem.Instance.scanPoint;
+        checkPointData.energyPoint = CollectionSystem.Instance.energyPoint;
 
         //checkPointData.SetCheckPointData();
-        checkPoints.Push(checkPointData);
+        checkPoints.Add(currentCheckPointName,checkPointData);
     }
-    public CheckPointData LoadCheckPoint()
+    public void LoadCheckPoint()
     {
-        return checkPoints.Pop();
+        if (checkPoints.ContainsKey(currentCheckPointName))
+        {
+            CheckPointData checkPointData = checkPoints[currentCheckPointName];
+            PlayerView.Instance.Reset(checkPointData);
+            CollectionSystem.Instance.Reset(checkPointData);
+        }
+        else
+        {
+            Debug.LogWarning("没有可用的检查点数据！");
+        }
     }
 }
