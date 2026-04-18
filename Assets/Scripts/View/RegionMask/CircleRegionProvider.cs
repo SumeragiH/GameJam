aeb199b2d42b4869ee4050ca5acbec1b8607f6aa
yuidaisuki <1641636977@qq.com>
2026-04-18 14:58:47 +0800

    安全区扩大与缩小

--- Assets/Scripts/View/RegionMask/CircleRegionProvider.cs
+++ Assets/Scripts/View/RegionMask/CircleRegionProvider.cs
@@ -4,6 +4,46 @@ public class CircleRegionProvider : RegionProviderBase
 {
     [SerializeField] private Transform _center;
     [SerializeField, Min(0f)] private float _radiusWorld = 2f;
+    [SerializeField] private bool _isZoomed;
+    [SerializeField, Min(0.01f)] private float _zoomTransitionDuration = 0.2f;
+
+    private float _zoomedScale = 1f;
+    private float _currentScale = 1f;
+
+    public bool IsZoomed
+    {
+        get => _isZoomed;
+        set => _isZoomed = value;
+    }
+
+    public void SetZoomScale(float zoomedScale)
+    {
+        _zoomedScale = Mathf.Max(1f, zoomedScale);
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        _currentScale = GetTargetScale();
+    }
+
+    private void Update()
+    {
+        float targetScale = GetTargetScale();
+        float speed = 1f / Mathf.Max(0.01f, _zoomTransitionDuration);
+        _currentScale = Mathf.MoveTowards(_currentScale, targetScale, speed * Time.deltaTime);
+    }
+
+    private void OnValidate()
+    {
+        _zoomedScale = Mathf.Max(1f, _zoomedScale);
+        _zoomTransitionDuration = Mathf.Max(0.01f, _zoomTransitionDuration);
+
+        if (!Application.isPlaying)
+        {
+            _currentScale = GetTargetScale();
+        }
+    }
 
     protected override bool TryBuildRegionData(Camera camera, out RegionShaderData data)
     {
@@ -20,13 +60,14 @@ public class CircleRegionProvider : RegionProviderBase
         float aspect = GetAspectCorrection(camera);
         Vector2 centerViewport = ApplyAspectCorrection(new Vector2(centerViewport3.x, centerViewport3.y), aspect);
 
-        Vector3 edgeWorld = centerWorld + camera.transform.right * _radiusWorld;
+        float scaledRadiusWorld = _radiusWorld * _currentScale;
+        Vector3 edgeWorld = centerWorld + camera.transform.right * scaledRadiusWorld;
         Vector2 edgeViewport = ApplyAspectCorrection(ToViewport2(camera, edgeWorld), aspect);
         float radiusViewport = (edgeViewport - centerViewport).magnitude;
 
         if (radiusViewport <= 0f)
         {
-            Vector3 fallbackEdgeWorld = centerWorld + camera.transform.up * _radiusWorld;
+            Vector3 fallbackEdgeWorld = centerWorld + camera.transform.up * scaledRadiusWorld;
             Vector2 fallbackEdgeViewport = ApplyAspectCorrection(ToViewport2(camera, fallbackEdgeWorld), aspect);
             radiusViewport = (fallbackEdgeViewport - centerViewport).magnitude;
         }
@@ -47,5 +88,10 @@ public class CircleRegionProvider : RegionProviderBase
             SkewTangent = 0f
         };
         return true;
+    }
+
+    private float GetTargetScale()
+    {
+        return _isZoomed ? _zoomedScale : 1f;
     }
 }
