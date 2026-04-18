6ee0218d0c238a1039adbfa406315db7cbf5588c
SumeragiH <1443490671@qq.com>
2026-04-18 13:44:51 +0800

    角色和UI

--- Assets/Scripts/View/UIView/InitPanel.cs
+++ Assets/Scripts/View/UIView/InitPanel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InitPanel : MonoBehaviour
+public class InitPanel : BasePanel
 {
     public Button btnStart;
     public Button btnEnd;
