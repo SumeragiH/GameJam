46e6ef0e18e57c79ee8ac1fbcd1cb88c7d0fa0c8
SumeragiH <1443490671@qq.com>
2026-04-19 21:09:16 +0800

    Revert "Merge branch 'main' of github.com:SumeragiH/GameJam"
    This reverts commit 738997813f6ebeab74404925064028c2ccaf5dd2, reversing
    changes made to 819ff08ea8aa2584c949577328c0c05b937910ff.

--- Assets/Scripts/View/UIView/EnergyBar.cs
+++ Assets/Scripts/View/UIView/EnergyBar.cs
@@ -8,7 +8,7 @@ public class EnergyBar : MonoBehaviour
 
     // 显示/隐藏的锚点坐标（按你的UI布局调整）
     public Vector2 showPos = Vector2.zero;
-    public Vector2 hidePos = new Vector2(-92, 0);
+    public Vector2 hidePos = new Vector2(-80, 0);
 
     public void Show(float time, UnityAction action = null)
     {
